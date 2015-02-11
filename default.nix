59e9e1baf530e430b71dd2f2e067781b7b07109d
Henry Till <henrytill@gmail.com>
2015-02-10 19:14:05 -0500

    add plugin paths

--- default.nix
+++ default.nix
@@ -174,6 +174,14 @@ in
         then [ pkgs.ffado ]
         else [];
 
+    environment.variables = {
+      VST_PATH = "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst";
+      LXVST_PATH = "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
+      LADSPA_PATH = "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa";
+      LV2_PATH = "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2";
+      DSSI_PATH = "$HOME/.dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi";
+    };
+
     powerManagement.cpuFreqGovernor = "performance";
 
     security.pam.loginLimits = [
