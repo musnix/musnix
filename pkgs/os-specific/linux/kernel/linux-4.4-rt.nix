af57d64350bc142bfe989e9821502000b2507610
Henry Till <henrytill@gmail.com>
2016-07-30 19:06:15 -0400

    kernel: 4.4.12-rt19 -> 4.4.15-rt23

--- pkgs/os-specific/linux/kernel/linux-4.4-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4-rt.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  kversion = "4.4.12";
-  pversion = "rt19";
+  kversion = "4.4.15";
+  pversion = "rt23";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.4";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
-    sha256 = "1r96jyvm44615f5zh5sn04zx7y8bllpx12lx1zjkns66i4ddv0rq";
+    sha256 = "1c5ccki68s5207ccg07h8pplcsz2kgsnkbpsvdpq0f8g3xqk8dih";
   };
 
   kernelPatches = args.kernelPatches;
