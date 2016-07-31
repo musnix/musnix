1b64078cb0e1fc40e85ee80c5f2dd7e3c3624ef2
Henry Till <henrytill@gmail.com>
2016-07-31 14:50:24 -0400

    kernel: 3.14.65-rt68 -> 3.14.73-rt78

--- pkgs/os-specific/linux/kernel/patches.nix
+++ pkgs/os-specific/linux/kernel/patches.nix
@@ -19,9 +19,9 @@ in rec {
 
   realtimePatch_3_14 = realtimePatch
     { branch = "3.14";
-      kversion = "3.14.65";
-      pversion = "rt68";
-      sha256 = "1xzwk1v7iv82wkdj4n3vclha1vz2678qqkczrvsx0apdxqcnpp8c";
+      kversion = "3.14.73";
+      pversion = "rt78";
+      sha256 = "0fxqh4vdgzkl5jfc231lh7zmqy6s1ygx8brxw0ja7v4hh09rfshz";
     };
 
   realtimePatch_3_18 = realtimePatch
