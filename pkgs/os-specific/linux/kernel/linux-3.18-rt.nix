14ab0d6a2930cc3088f61c09a69e37bd556c7973
Henry Till <henrytill@gmail.com>
2016-07-31 03:16:48 -0400

    kernel: 3.18.29-rt30 -> 3.18.36-rt38

--- pkgs/os-specific/linux/kernel/linux-3.18-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-3.18-rt.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  kversion = "3.18.29";
-  pversion = "rt30";
+  kversion = "3.18.36";
+  pversion = "rt38";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "3.18";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v3.x/linux-${kversion}.tar.xz";
-    sha256 = "0g8vlhifl31dyghiamykrpgj6n8h5w6gh6n88ir57z6lj188vaj8";
+    sha256 = "0iqyll1p1pkyl5rj440kjg483gqhhg6z7r61ln6rzbqm5g943fvq";
   };
 
   kernelPatches = args.kernelPatches;
