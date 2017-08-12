9dae0e120461643137b19d6d8eb8cd5f68b1e710
Bart Brouns <bart@magnetophon.nl>
2017-08-13 01:21:25 +0200

    linux: 4.11.9-rt7 -> 4.11.12-rt9

--- pkgs/os-specific/linux/kernel/linux-4.11-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.11-rt.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, hostPlatform, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  kversion = "4.11.9";
-  pversion = "rt7";
+  kversion = "4.11.12";
+  pversion = "rt9";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.11";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
-    sha256 = "0q60690hmqhz2x3v6qyjq7lhp2j99dcldvd46myc9ggp78d93j1z";
+    sha256 = "14k10g9w8dp3lmw1qjns395a2fcaq2iw1jijss5npxllh3hx8drf";
   };
 
   kernelPatches = args.kernelPatches;
