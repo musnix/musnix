c839069cf3db8f2549377be63dac504d1305b2a2
Bart Brouns <bart@magnetophon.nl>
2017-09-29 14:33:19 +0200

    kernel: 4.11.12-rt11 -> 4.11.12-rt14

--- pkgs/os-specific/linux/kernel/linux-4.11-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.11-rt.nix
@@ -2,7 +2,7 @@
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
   kversion = "4.11.12";
-  pversion = "rt11";
+  pversion = "rt14";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.11";
 
