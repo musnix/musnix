159bd839f1ee14ba9789d2a88c9a9556722da03f
Bart Brouns <bart@magnetophon.nl>
2017-10-26 12:21:55 +0200

    kernel: add 4.13.7-rt1

---
+++ pkgs/os-specific/linux/kernel/linux-4.13-rt.nix
@@ -0,0 +1,20 @@
+{ stdenv, fetchurl, hostPlatform, perl, buildLinux, ... } @ args:
+
+import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
+  kversion = "4.13.7";
+  pversion = "rt1";
+  version = "${kversion}-${pversion}";
+  extraMeta.branch = "4.11";
+
+  src = fetchurl {
+    url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
+    sha256 = "16vjjl3qw0a8ci6xbnywhb8bpr3ccbs0i6xa54lc094cd5gvx4v3";
+  };
+
+  kernelPatches = args.kernelPatches;
+
+  features.iwlwifi = true;
+  features.efiBootStub = true;
+  features.needsCifsUtils = true;
+  features.netfilterRPFilter = true;
+} // (args.argsOverride or {}))
