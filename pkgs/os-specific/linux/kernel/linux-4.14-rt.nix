844ef2ca355b1f8877d0ab7c2dabf63b38d095cf
Bart Brouns <bart@magnetophon.nl>
2017-12-21 14:56:08 +0100

    kernel 4.14.3-rt5 -> 4.14.6-rt7

--- pkgs/os-specific/linux/kernel/linux-4.14-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14-rt.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, hostPlatform, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  kversion = "4.14.3";
-  pversion = "rt5";
+  kversion = "4.14.6";
+  pversion = "rt7";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
-    sha256 = "0rh3r1ik9xwa3pl5rrqjjbknyc7xnam10jih7v5q5v33pd716a5n";
+    sha256 = "1k3c6kmb7zwyh1ga3jb1bimqjszmcdihn2ndvvdns57am65nf1q9";
   };
 
   kernelPatches = args.kernelPatches;
