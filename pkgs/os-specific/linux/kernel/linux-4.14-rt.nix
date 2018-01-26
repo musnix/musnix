624b31571311be1fec7f506da5babfd7c9c45249
Bart Brouns <bart@magnetophon.nl>
2018-01-26 14:30:34 +0100

    kernel: 4.14.12-rt10 -> 4.14.15-rt13

--- pkgs/os-specific/linux/kernel/linux-4.14-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14-rt.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchurl, hostPlatform, buildPackages, perl, buildLinux, ... } @ args:
 
 import <nixpkgs/pkgs/os-specific/linux/kernel/generic.nix> (args // rec {
-  kversion = "4.14.12";
-  pversion = "rt10";
+  kversion = "4.14.15";
+  pversion = "rt13";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
-    sha256 = "1bsn73h3ilf7msyiqm5ny2zdj30b9r7k9sc8i03w3iggh3agf236";
+    sha256 = "0hk15qslkq15x53zkp70gnhdmjg5j9xigyykmig3g03gqsh97hzz";
   };
 
   kernelPatches = args.kernelPatches;
