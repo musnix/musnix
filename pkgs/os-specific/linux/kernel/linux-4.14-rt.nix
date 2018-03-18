7f6835678e7709352557afc77c1b474cb1396227
Bart Brouns <bart@magnetophon.nl>
2018-03-18 09:35:27 +0100

    kernel: 4.14.24-rt19 -> 4.14.27-rt21

--- pkgs/os-specific/linux/kernel/linux-4.14-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.14-rt.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, hostPlatform, buildPackages, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  kversion = "4.14.24";
-  pversion = "rt19";
+  kversion = "4.14.27";
+  pversion = "rt21";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.14";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
-    sha256 = "00cqhc8198f4gj6cpz7nblpgi5zh2145arjx1yp0p4gmswdjslds";
+    sha256 = "1si8l3clpkyhwawrjxz6yzx7xl0v0k6dy1yf5qiwf1hsqx4s8489";
   };
 } // (args.argsOverride or {}))
