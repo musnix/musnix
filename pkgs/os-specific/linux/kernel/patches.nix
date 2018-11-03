9a547b159c12ad6eaa2509d89fa3cd0b2ea1046d
Bart Brouns <bart@magnetophon.nl>
2018-11-03 17:11:17 +0100

    kernel: 4.18.7-rt5 -> 4.18.16-rt9 (#87)
    * kernel: 4.18.7-rt5 -> 4.18.12-rt7
    
    * kernel: 4.18.12-rt7 -> 4.18.16-rt8
    
    * kernel: 4.18.16-rt8 -> 4.18.16-rt9

--- pkgs/os-specific/linux/kernel/patches.nix
+++ pkgs/os-specific/linux/kernel/patches.nix
@@ -75,8 +75,8 @@ in rec {
 
   realtimePatch_4_18 = realtimePatch
     { branch = "4.18";
-      kversion = "4.18.7";
-      pversion = "rt5";
-      sha256 = "0205lnacz3f16vddbzj8z8h9arf4xxznaq3ahp94lwlas2djq1k2";
-    };
+      kversion = "4.18.16";
+    pversion = "rt9";
+    sha256 = "04zzm1mlm9km1776w9bgblyr6c36wc10wmyvkf4pimwdd7ch50mb";
+  };
 }
