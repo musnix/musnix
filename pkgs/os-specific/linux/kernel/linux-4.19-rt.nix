37d9eeb7d3542cc4be27af2a018426fed19df97f
Bart Brouns <bart@magnetophon.nl>
2020-06-06 18:32:14 +0200

    kernels: 4.19.31-rt18 -> 4.19.124-rt53

--- pkgs/os-specific/linux/kernel/linux-4.19-rt.nix
+++ pkgs/os-specific/linux/kernel/linux-4.19-rt.nix
@@ -1,13 +1,13 @@
 { stdenv, fetchurl, hostPlatform, buildPackages, perl, buildLinux, ... } @ args:
 
 buildLinux (args // rec {
-  kversion = "4.19.31";
-  pversion = "rt18";
+  kversion = "4.19.124";
+  pversion = "rt53";
   version = "${kversion}-${pversion}";
   extraMeta.branch = "4.19";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/v4.x/linux-${kversion}.tar.xz";
-    sha256 = "02r822zhs1xcjy6jpf9fv0h4br47drd3xssxapr2b45y8anr1aks";
+    sha256 = "005dznldnj1m03cbkc5pd2q2cv9jj1j6a0x2vh4p79ypg4c01nfm";
   };
 } // (args.argsOverride or {}))
