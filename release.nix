7f67d6868a76669f45a7e4c6e8272ee75d9b8656
Henry Till <henrytill@gmail.com>
2020-07-29 14:35:41 -0400

    remove broken kernels

--- release.nix
+++ release.nix
@@ -3,14 +3,9 @@ let
 
   jobs = rec {
     musnixTest    = import ./tests/default.nix;
-    linux_3_18_rt = pkgs.linux_3_18_rt;
-    linux_4_1_rt  = pkgs.linux_4_1_rt;
     linux_4_4_rt  = pkgs.linux_4_4_rt;
     linux_4_9_rt  = pkgs.linux_4_9_rt;
-    linux_4_11_rt = pkgs.linux_4_11_rt;
-    linux_4_13_rt = pkgs.linux_4_13_rt;
     linux_4_14_rt = pkgs.linux_4_14_rt;
-    linux_4_16_rt = pkgs.linux_4_16_rt;
     linux_4_18_rt = pkgs.linux_4_18_rt;
     linux_4_19_rt = pkgs.linux_4_19_rt;
     linux_5_0_rt  = pkgs.linux_5_0_rt;
