f2698563c25f38e722b64cc2108ab85f11c9dec5
cidkidnix <cidkidnix@protonmail.com>
2021-06-17 07:22:51 -0500

    flake: We don't need forAllSystems

--- flake.nix
+++ flake.nix
@@ -1,13 +1,7 @@
 {
     description = "Real-time audio in NixOS";
     inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    outputs = { self, nixpkgs }: let
-        systems = [
-            "x86_64-linux"
-            "aarch64-linux"
-        ];
-        forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in {
+    outputs = { self, nixpkgs }: {
         nixosModules.musnix = import ./default.nix;
         nixosModule = self.nixosModules.musnix;
     };
