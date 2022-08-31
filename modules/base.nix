c28a81cfdc33cbe95bce3aa853da5d8e5d8f5d00
bandthedoge <bandithedoge@protonmail.com>
2022-08-31 19:14:25 +0200

    support plugin paths installed by home-manager

--- modules/base.nix
+++ modules/base.nix
@@ -65,13 +65,21 @@ in {
         then [ pkgs.ffadoFull ]
         else [];
 
-    environment.variables = {
-      DSSI_PATH   = "$HOME/.dssi:$HOME/.nix-profile/lib/dssi:/run/current-system/sw/lib/dssi";
-      LADSPA_PATH = "$HOME/.ladspa:$HOME/.nix-profile/lib/ladspa:/run/current-system/sw/lib/ladspa";
-      LV2_PATH    = "$HOME/.lv2:$HOME/.nix-profile/lib/lv2:/run/current-system/sw/lib/lv2";
-      LXVST_PATH  = "$HOME/.lxvst:$HOME/.nix-profile/lib/lxvst:/run/current-system/sw/lib/lxvst";
-      VST_PATH    = "$HOME/.vst:$HOME/.nix-profile/lib/vst:/run/current-system/sw/lib/vst";
-      VST3_PATH   = "$HOME/.vst3:$HOME/.nix-profile/lib/vst3:/run/current-system/sw/lib/vst3";
+    environment.variables = let
+      makePluginPath = format:
+        (makeSearchPath format [
+          "$HOME/.nix-profile/lib"
+          "/run/current-system/sw/lib"
+          "/etc/profiles/per-user/$USER/lib"
+        ])
+        + ":$HOME/.${format}";
+    in {
+      DSSI_PATH   = makePluginPath "dssi";
+      LADSPA_PATH = makePluginPath "ladspa";
+      LV2_PATH    = makePluginPath "lv2";
+      LXVST_PATH  = makePluginPath "lxvst";
+      VST_PATH    = makePluginPath "vst";
+      VST3_PATH   = makePluginPath "vst3";
     };
 
     powerManagement.cpuFreqGovernor = "performance";
