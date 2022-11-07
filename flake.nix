83efe9ff1e02d437da63816601a9a979e78890b4
Henry Till <henrytill@gmail.com>
2022-11-07 12:39:06 -0800

    flake.{nix,lock}: update

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-    description = "Real-time audio in NixOS";
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    outputs = { self, nixpkgs }: {
-        nixosModules.musnix = import ./default.nix;
-        nixosModule = self.nixosModules.musnix;
-    };
+  description = "Real-time audio in NixOS";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }: {
+    nixosModules.musnix = import ./default.nix;
+    nixosModule = self.nixosModules.musnix;
+  };
 }
