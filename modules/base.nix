f494efdeff77c20c02d5c9d78039cea7c04ce8bf
Chris McDonough <chrism@plope.com>
2024-03-31 01:52:23 -0400

    add udev rule to allow users in the audio group to change cpu dma latency

--- modules/base.nix
+++ modules/base.nix
@@ -99,6 +99,7 @@ in {
       extraRules = ''
         KERNEL=="rtc0", GROUP="audio"
         KERNEL=="hpet", GROUP="audio"
+        DEVPATH=="/devices/virtual/misc/cpu_dma_latency", OWNER="root", GROUP="audio", MODE="0660"
       '';
     };
   };
