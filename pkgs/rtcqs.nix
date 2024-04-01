15c51d62cb67eafda996aa06c62645eae72c5352
Chris McDonough <chrism@plope.com>
2024-04-01 01:58:32 -0400

    add dep on pysimplegui, which is required by rtcqs_gui

--- pkgs/rtcqs.nix
+++ pkgs/rtcqs.nix
@@ -7,9 +7,11 @@ pkgs.python3.pkgs.buildPythonApplication rec {
     buildInputs = [
       pkgs.python3.pkgs.setuptools
     ];
+    propagatedBuildInputs = [
+      pkgs.python3.pkgs.pysimplegui
+    ];
     src = fetchPypi {
       inherit pname version;
       hash = "sha256-DfeV9kGhdMf6hZ1iNJ0L3HUn7m8c1gRK5cjtJNUAvJI=";
     };
 }
-  
