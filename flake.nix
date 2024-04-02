0dc4317bcad7986def1059ce12e43b530607e58c
Chris McDonough <chrism@plope.com>
2024-04-02 05:01:17 -0400

    Update actions, use nix build rather than nix-build, disuse channel (its all flakes), dont pretend we are testing aarch64

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Real-time audio in NixOS";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: let
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
     in {
       nixosModules.musnix = import ./default.nix;
       nixosModules.default = self.nixosModules.musnix;
