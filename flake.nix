46055b5472efded26a3bc8cf6c12adca0a8bd51a
Chris McDonough <chrism@plope.com>
2024-04-02 00:37:46 -0400

    suggest nix flake check -L instead of nix-build tests/default.nix to ensure everyone is running tests against the same nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,8 +1,18 @@
 {
   description = "Real-time audio in NixOS";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
-    nixosModules.musnix = import ./default.nix;
-    nixosModules.default = self.nixosModules.musnix;
+  outputs = { self, nixpkgs }: let
+      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
+    in {
+      nixosModules.musnix = import ./default.nix;
+      nixosModules.default = self.nixosModules.musnix;
+      checks = forAllSystems (system: let
+        checkArgs = {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit self;
+        };
+      in {
+        default = import ./tests/default.nix checkArgs;
+      });
   };
 }
