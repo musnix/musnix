c0c8768809a75d60a348925be56540cd680260e9
Chris McDonough <chrism@plope.com>
2024-04-03 20:09:54 -0400

    Use flake check for tests (#176)
    * older nixpkgs incompatible with new kernel patches
    
    * suggest nix flake check -L instead of nix-build tests/default.nix to ensure everyone is running tests against the same nixpkgs
    
    * provide breadcrumbs for users of nix-build
    
    * try to free up some disk space such that check can pass
    
    * Update actions, use nix build rather than nix-build, disuse channel (its all flakes), dont pretend we are testing aarch64
    
    * apparently even 'nix build' requires a channel

---
+++ tests/lib.nix
@@ -0,0 +1,30 @@
+# see https://blog.thalheim.io/2023/01/08/how-to-use-nixos-testing-framework-with-flakes/
+
+# The first argument to this function is the test module itself
+test:
+
+# These arguments are provided by `flake.nix` on import, see checkArgs
+# Use "nix flake check -L" instead of "nix-build test/default.nix if you see this in an error
+{ pkgs, self}:
+
+let
+  inherit (pkgs) lib;
+
+  # this imports the nixos library that contains our testing framework
+  nixos-lib = import (pkgs.path + "/nixos/lib") {};
+in
+(nixos-lib.runTest {
+  hostPkgs = pkgs;
+
+  # This speeds up the evaluation by skipping evaluating documentation
+  # (optional)
+  defaults.documentation.enable = lib.mkDefault false;
+
+  # This makes `self` available in the NixOS configuration of our virtual
+  # machines. This is useful for referencing modules or packages from your own
+  # flake as well as importing from other flakes.
+  node.specialArgs = { inherit self; };
+
+  imports = [ test ];
+}).config.result
+
